3037b4c54520a819af815676569d13668a891db4
Mouthless-Stoat <nphuyanh@outlook.com>
2026-03-10 22:50:56 -0300

    yay more boilerplate

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,85 @@
-struct FightManager<Sigil> {
-    board: Board,
-    cards: Vec<Card<Sigil>>,
+pub struct FightManager<Sigil> {
+    pub board: Board,
+    pub cards: Vec<Card<Sigil>>,
 }
 
-struct Player {
+#[derive(Clone, Copy)]
+pub enum PlayerID {
+    First,
+    Second,
+}
+
+pub struct Player {
     hand: Vec<CardID>,
 }
 
-struct Board {
-    first: Vec<Slot>,
-    second: Vec<Slot>,
-    scale: f64,
+pub struct Board {
+    pub first: Vec<Slot>,
+    pub second: Vec<Slot>,
+    pub scale: f64,
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-struct CardID(isize);
+pub struct CardID(isize);
 
-struct Slot {
-    card_id: CardID,
+pub struct Slot {
+    pub card_id: CardID,
 }
 
-struct Card<Sigil> {
-    id: CardID,
-    name: String,
-    attack: f64,
-    health: f64,
-    sigils: Vec<Sigil>,
+pub struct Card<Sigil> {
+    pub id: CardID,
+    pub name: String,
+    pub attack: f64,
+    pub health: f64,
+    pub sigils: Vec<Sigil>,
 }
 
 impl<Sigil> FightManager<Sigil> {
-    fn get_mut_from_id(&mut self, id: CardID) -> Option<&mut Card<Sigil>> {
+    pub fn get_mut_from_id(&mut self, id: CardID) -> Option<&mut Card<Sigil>> {
         self.cards.iter_mut().find(|card| card.id == id)
     }
+
+    pub fn get_from_id(&mut self, id: CardID) -> Option<&Card<Sigil>> {
+        self.cards.iter().find(|card| card.id == id)
+    }
+}
+
+/// Default Inscrytion Vanilla Sigil
+#[derive(Clone, Debug)]
+pub enum VanillaSigil {
+    BifucatedStrike,
+    BoneKing,
+    BombLatch,
+    Amorphorus,
+    RabbitHole,
+    DamBuilder,
+    BeesWithin,
+    LooseTails,
+    Sprinter,
+    Waterborne,
+    Guardian,
+    Burrower,
+}
+
+pub trait Sigil {
+    fn handle(self, card: CardID, ctx: Context);
+}
+
+pub struct Context {
+    pub event: SigilEvent,
+    pub cause: CardID,
+}
+
+#[derive(Clone)]
+#[allow(clippy::enum_variant_names, missing_docs)]
+pub enum SigilEvent {
+    OnAttack,
+    OnActivate,
+    OnDeath,
+    OnDraw,
+    OnPlay,
+    OnSacrifice,
+    OnDamage,
+    OnTurnEnd,
+    OnPlayerStarve(PlayerID),
 }
