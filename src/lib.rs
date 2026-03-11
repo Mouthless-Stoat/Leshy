71bc85d9ea489c6314480ce013e4ed4bfccde638
Mouthless-Stoat <nphuyanh@outlook.com>
2026-03-11 16:38:46 -0300

    desing change and more boilerplate
    MOce the ID into a new struct to bundle the information along with other

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,16 @@
-pub struct FightManager<Sigil> {
+pub struct FightManager<S: Sigil> {
     pub board: Board,
-    pub cards: Vec<Card<Sigil>>,
+    pub cards: Vec<CardData<S>>,
+    pub active_player: PlayerID,
+    pub players: (Player<S>, Player<S>),
+}
+
+/// A bundle of a raw card with its ID and wherever if the card is on the board. The FightManager
+/// uses this to handle one master copy of a card that other function borrow from.
+pub struct CardData<S: Sigil> {
+    id: CardID,
+    card: Card<S>,
+    on_board: bool,
 }
 
 #[derive(Clone, Copy)]
@@ -9,8 +19,9 @@ pub enum PlayerID {
     Second,
 }
 
-pub struct Player {
+pub struct Player<S: Sigil> {
     hand: Vec<CardID>,
+    deck: Vec<Card<S>>,
 }
 
 pub struct Board {
@@ -26,21 +37,50 @@ pub struct Slot {
     pub card_id: CardID,
 }
 
-pub struct Card<Sigil> {
-    pub id: CardID,
+/// The raw card data that store all the information that a card might have.
+pub struct Card<S: Sigil> {
     pub name: String,
     pub attack: f64,
     pub health: f64,
-    pub sigils: Vec<Sigil>,
+    pub sigils: Vec<S>,
 }
 
-impl<Sigil> FightManager<Sigil> {
-    pub fn get_mut_from_id(&mut self, id: CardID) -> Option<&mut Card<Sigil>> {
-        self.cards.iter_mut().find(|card| card.id == id)
+impl<S: Sigil> FightManager<S> {
+    pub fn get_mut_from_id(&mut self, id: CardID) -> Option<&mut Card<S>> {
+        self.cards
+            .iter_mut()
+            .find(|card| card.id == id)
+            .map(|cd| &mut cd.card)
     }
 
-    pub fn get_from_id(&mut self, id: CardID) -> Option<&Card<Sigil>> {
-        self.cards.iter().find(|card| card.id == id)
+    pub fn get_from_id(&mut self, id: CardID) -> Option<&Card<S>> {
+        self.cards
+            .iter()
+            .find(|card| card.id == id)
+            .map(|cd| &cd.card)
+    }
+
+    pub fn draw_card(player: PlayerID) {}
+
+    pub fn activation_order(self) -> Vec<CardID> {
+        match self.active_player {
+            PlayerID::First => self
+                .players
+                .0
+                .hand
+                .iter()
+                .cloned()
+                .chain(self.players.1.hand.clone())
+                .collect(),
+            PlayerID::Second => self
+                .players
+                .1
+                .hand
+                .iter()
+                .cloned()
+                .chain(self.players.0.hand.clone())
+                .collect(),
+        }
     }
 }
 
@@ -61,8 +101,8 @@ pub enum VanillaSigil {
     Burrower,
 }
 
-pub trait Sigil {
-    fn handle(self, card: CardID, ctx: Context);
+pub trait Sigil: Sized {
+    fn handle(self, fight_manager: FightManager<Self>, card: CardID, ctx: Context);
 }
 
 pub struct Context {
